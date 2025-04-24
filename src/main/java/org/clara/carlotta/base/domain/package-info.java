a9b214498cf8df3a8a0c8f6ef98f3433e2c5a433
start.vaadin.com <no-reply@vaadin.com>
2025-04-24 14:57:06 +0000

    Generated project

---
+++ src/main/java/org/clara/carlotta/base/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable domain classes.
+ */
+@NullMarked
+package org.clara.carlotta.base.domain;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/org/clara/carlotta/base/ui/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable UI components.
+ */
+@NullMarked
+package org.clara.carlotta.base.ui.component;
+
+import org.jspecify.annotations.NullMarked;
